4aa8348998218941dd9ca161e89981c38d348448
Neary <kibishiigaming@gmail.com>
2025-03-04 10:19:21 +0100

    Fond étoilé mise en place.

---
+++ mon-projet/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Background from "./components/background";
+import "./App.css"; // Si tu as un fichier CSS global
+
+const App = () => {
+    return (
+    <div className="App">
+        <Background />
+      {/* Ton contenu ici */}
+    </div>
+    );
+};
+
+export default App;
+
+
+
---
+++ mon-projet/src/components/background.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/background.css";
+
+const Background = () => {
+    return (
+    <div className="background">
+        <div className="background-layer"></div>
+    </div>
+    );
+};
+
+export default Background;
